1fc02c06dc7392496e15b264c5a05a57f18bed30
Khai Minh Ma <32994111+khaiminh302@users.noreply.github.com>
2022-03-10 23:01:40 +0700

    Teamwork merge

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.onboarding_one)
+//        setContentView(R.layout.sign_up)
+//        setContentView(R.layout.login)
+//        setContentView(R.layout.verification)
     }
 }
