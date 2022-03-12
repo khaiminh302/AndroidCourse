cfdef1c9e550e3b397efaac771123c6c1aaed9b8
Khai Minh Ma <32994111+khaiminh302@users.noreply.github.com>
2022-03-12 16:49:43 +0700

    Week2 app init

---
+++ Week2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_onboarding_one)
+//        setContentView(R.layout.activity_sign_up)
+//        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.activity_verification)
+    }
+}
