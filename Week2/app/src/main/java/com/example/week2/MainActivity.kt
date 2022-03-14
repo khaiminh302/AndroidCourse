8d2a0db13f309471ec322c1b01092494f7b9c4e1
Khai Minh Ma <32994111+khaiminh302@users.noreply.github.com>
2022-03-14 16:14:19 +0700

    Update

--- Week2/app/src/main/java/com/example/week2/MainActivity.kt
+++ Week2/app/src/main/java/com/example/week2/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.week2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        setContentView(R.layout.activity_onboarding_one)
+//        setContentView(R.layout.activity_onboarding_1)
+//        setContentView(R.layout.activity_onboarding_2)
+//        setContentView(R.layout.activity_onboarding_3)
+
+//        setContentView(R.layout.activity_welcome)
+
 //        setContentView(R.layout.activity_sign_up)
 //        setContentView(R.layout.activity_login)
 //        setContentView(R.layout.activity_verification)
+
+
+
     }
 }
