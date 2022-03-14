c04b1404b81fdf5e3564a15f94acc4b0188f762e
Khai Minh Ma <32994111+khaiminh302@users.noreply.github.com>
2022-03-14 22:02:52 +0700

    Clean up and add login activities

--- Week2/app/src/main/java/com/example/week2/SignUpActivity.kt
+++ Week2/app/src/main/java/com/example/week2/SignUpActivity.kt
@@ -1,11 +1,19 @@
 package com.example.week2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val login = findViewById<TextView>(R.id.login)
+        login.setOnClickListener {
+            val intent = Intent (this@SignUpActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Week2/app/src/main/java/com/example/week2/WelcomeActivity.kt
+++ Week2/app/src/main/java/com/example/week2/WelcomeActivity.kt
@@ -3,12 +3,14 @@ package com.example.week2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("WELCOME", "onCreate")
         setContentView(R.layout.activity_welcome)
 
         val start_sign_up = findViewById<Button>(R.id.start_w_mail_phone)
@@ -23,5 +25,30 @@ class WelcomeActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("WELCOME", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("WELCOME", "onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("WELCOME", "onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("WELCOME", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("WELCOME", "onStop")
     }
 }
